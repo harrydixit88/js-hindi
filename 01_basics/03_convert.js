c71cf94c5f06b35597d545c2e6368ddadbc8a32a
harrydixit88 <143481303+harrydixit88@users.noreply.github.com>
2023-08-29 10:14:58 +0000

    primitive heap

---
+++ 01_basics/03_convert.js
@@ -0,0 +1,54 @@
+//let someNumber = 33
+
+//let stringNumber = String(someNumber)
+//console.log(stringNumber);
+
+
+//##########Optraion######
+
+
+/*let str1 = "hello"
+let str2 = " harry"
+let str3 = str1 + str2 
+
+console.log(str3);
+*/
+
+
+// # primitive 
+// # 7 type : string,number,boolean,null,undifined,symbol,bigint
+
+/*const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+const id = Symbol('123')
+const anotherId = Symbol('123')*/
+
+
+// reference type ( non primitive)
+
+//array, object,function
+
+const heros = ["shaktiman","naagraj","doremon"];
+let myobj = {
+    name: "harry",
+    age: 26,
+}
+
+
+//================================memory
+// stack (primitive), heap (non-primitive)
+let myname = "harry"
+
+let anothername = myname
+anothername = "pandit"
+
+console.log(myname);
+console.log(anothername);
+
+
+
+
