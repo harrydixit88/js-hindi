33ddd216817f07fa301092e241e2067c00390bd4
harrydixit88 <143481303+harrydixit88@users.noreply.github.com>
2023-08-29 07:12:11 +0000

    variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountID = 144553
+let accountemail = "harrydixit@gmail.com"
+var accountpassword = "12345"
+accountcity = "gurgaon"
+
+
+//accountID = 2 not allowed
+
+accountemail = "krishna@gmail.com"
+accountpassword = "2121"
+
+
+
+console.log(accountID);
+console.table([accountemail,accountpassword]);
